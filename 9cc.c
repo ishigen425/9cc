b10a76d660df232c5b227288d4bdbdc8efbb05e8
ishigen425 <g4.fine.yamanashi@gmail.com>
2020-10-30 22:10:44 +0900

    add error_at function

--- 9cc.c
+++ 9cc.c
@@ -22,31 +22,39 @@ struct Token {
 };
 
 Token *token;
+char *user_input;
 
-void error(char *fmt, ... ) {
+void error_at(char *loc, char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input - 1;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, " ^");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
 }
 
 bool consume(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op)
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
         return false;
+    }
     token = token->next;
     return true;
 }
 
 void expect(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        error_at(token->str, "'%c'ではありません", op); 
+    }
     token = token->next;
 }
 
 int expect_number() {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -86,7 +94,7 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -98,7 +106,8 @@ int main(int argc, char **argv) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
-
+    
+    user_input = argv[1];
     token = tokenize(argv[1]);
     
     printf(".intel_syntax noprefix\n");
