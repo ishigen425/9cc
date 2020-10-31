a92f57916978c119dd9fdd6213c368fabc47311c
ishigen425 <g4.fine.yamanashi@gmail.com>
2020-10-31 10:07:57 +0900

    step6

--- 9cc.c
+++ 9cc.c
@@ -124,6 +124,7 @@ struct Node {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
     Node *node = calloc(1, sizeof(Node));
@@ -164,18 +165,25 @@ Node *primary() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
 }
 
+Node *unary(){
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), primary());
+    return primary();
+}
 
 void gen(Node *node) {
     if (node->kind == ND_NUM) {
